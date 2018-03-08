0389dbe381deacfb353b9b398e2955f3671db1c7
Minghao <minghao.wang@dbjtech.com>
2018-03-08 17:18:39 +0800

    feat: update webpack-learn

--- src/utils/utils.js
+++
@@ -1,3 +0,0 @@
-export function wordsToSentence(...words) {
-    return words.join(' ');
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,24 @@
-let HtmlWebpackPlugin = require('html-webpack-plugin'),
-  path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+    //定义了一些文件夹的路径
+const ROOT_PATH = path.resolve(__dirname);
+const APP_PATH = path.resolve(ROOT_PATH, 'app');
+const BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
+// 注：“__dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录。
 module.exports = {
-    entry: {
-        app: path.resolve(__dirname, 'src', 'index.js')
-    },
-    output: {
-        filename: '[name].js',
+    entry: APP_PATH, // 项目的文件夹 可以直接用文件夹名称 默认会找index.js 也可以确定是哪个文件名字
+    output: { // 让webpack编译完的文件放在哪里
+        filename: 'bundle.js', //输出的文件名 合并以后的js会命名为bundle.js
         path: path.resolve(__dirname, 'dist')
     },
-    plugins: [
+    plugins: [ // 放置不一样的插件,自动生成一个html文件
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'src', 'index.html')
         })
     ],
-    module: {
-        rules: [
-            {
+    module: { // 用不同的模块处理不一样的事情
+        rules: [{
                 test: /\.js$/,
                 loader: 'babel-loader',
                 include: path.resolve(__dirname, 'src')
@@ -27,5 +29,11 @@ module.exports = {
                 include: path.resolve(__dirname, 'src')
             }
         ]
-    }
+    },
+    devServer: { // webpack-dev-server 热更新的配置
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
+    },
 }
